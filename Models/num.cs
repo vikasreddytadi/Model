c866e361e63c9c44349b79547878515068220f0c
vikas <vikas.tadi@pal.tech>
2024-09-13 18:36:07 +0530

    async and await

--- Models/num.cs
+++ Models/num.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace model.Models
 {
     public class num
     {
+        // we can directly mention the name which will come from Json like below
+        // then while doing deserializing or serializing they will map
+        // automatically
+        // [JsonPropertyName("Id")]
         public int id { get; set; }
 
         public string name { get; set; }
--- Program.cs
+++ Program.cs
@@ -14,13 +14,18 @@ namespace model
     public class System
     {
         // When we create a property, it will automatically create a private field like _processor
-        static void Main(string[] args)
+        public async static Task asyncFunction(int delay, string name){
+                Console.WriteLine(name + "started");
+                await Task.Delay(delay);
+                Console.WriteLine(name + "completed");
+            }
+        static async Task Main(string[] args)
         {
-            IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appSettings.json")
-                .Build();
+            // IConfiguration config = new ConfigurationBuilder()
+            //     .AddJsonFile("appSettings.json")
+            //     .Build();
 
-            DapperMapper dapper = new DapperMapper();
+            // DapperMapper dapper = new DapperMapper();
 
             // DateTime result = dapper.SingleRow<DateTime>(@"Select GETDATE()");
 
@@ -55,39 +60,39 @@ namespace model
             // Computer myComputer = new Computer() { Processor = "Intel I7", Ram = 8 };
             // Console.WriteLine(myComputer.Processor);
 
-            string tempJson = File.ReadAllText("temp.json");
+            // string tempJson = File.ReadAllText("temp.json");
 
             // Console.WriteLine(tempJson);
 
-            JsonSerializerOptions options = new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
+            // JsonSerializerOptions options = new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // };
 
-            IEnumerable<numDto>? loadData = JsonSerializer.Deserialize<IEnumerable<numDto>>(
-                tempJson,
-                options
-            );
+            // IEnumerable<numDto>? loadData = JsonSerializer.Deserialize<IEnumerable<numDto>>(
+            //     tempJson,
+            //     options
+            // );
 
             // We need to set up mapper configuration
-            var config1 = new MapperConfiguration(
-                (cfg) =>
-                {
-                    cfg.CreateMap<numDto, num>()
-                        .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.name));
-                }
-            );
+            // var config1 = new MapperConfiguration(
+            //     (cfg) =>
+            //     {
+            //         cfg.CreateMap<numDto, num>()
+            //             .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.name));
+            //     }
+            // );
 
             // create mapper with above config
-            IMapper mapper = config1.CreateMapper();
+            // IMapper mapper = config1.CreateMapper();
 
             // here we will map two objects
-            IEnumerable<num> mappedData = mapper.Map<IEnumerable<num>>(loadData);
+            // IEnumerable<num> mappedData = mapper.Map<IEnumerable<num>>(loadData);
 
-            foreach (num mynum in mappedData)
-            {
-                Console.WriteLine(mynum.name);
-            }
+            // foreach (num mynum in mappedData)
+            // {
+            //     Console.WriteLine(mynum.name);
+            // }
 
             // if (loadData != null)
             // {
@@ -101,6 +106,17 @@ namespace model
             // string serializedJson = JsonSerializer.Serialize(loadData,options);
 
             // File.WriteAllText("serializedJson.txt",serializedJson);
+
+            // ---------- Async and Await ---------------
+            Task task1 = asyncFunction(100,"task1");
+            Task task2 = asyncFunction(300,"task2");
+            Task task3 = asyncFunction(50,"task3");
+            Task task4 = asyncFunction(50,"task4");
+
+            await task1;
+            await task2;
+            await task3;
+            await task4;            
         }
     }
 }
